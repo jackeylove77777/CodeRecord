d125f2d819611a5f0d34b832a74cae5c87e9eb63
jackeylove77777 <776109620@qq.com>
2022-04-10 00:23:16 +0800

    common

---
+++ CSharp11Feature.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+//C#11 新特性
+
+
+
+//C#11 列表模式新特性  （Rust出生自带）
+int[] nums = { 5, 4, 3, 2, 1, 3, 4, 5 };
+Console.WriteLine(isHuiWenArr(nums));//false
+int[] tmp = { 3, 2, 1, 2, 3 };
+Console.WriteLine(isHuiWenArr(tmp));//true
+
+
+bool isHuiWenArr(int[] arr)=>arr switch{
+    [] => false,
+    [_]=> true,
+    [var first, .. var mid, var last] => first == last ? isHuiWenArr(mid) : false,
+};
+try
+{
+    test();
+    Console.WriteLine(111);
+}
+catch (Exception e)
+{
+    Console.WriteLine();
+    Console.WriteLine(222);
+}//print 222
+
+//参数空值检查,!!标记的参数是null值，自动抛出异常
+void test(object obj!! = null) { };
+
+
+
+
+
+
+
+
+
+
+
